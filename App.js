7d322a2701d3d3e2c656ad33cb002e54b8ce3ae7
mrcodebs <singhbhupider206@gmail.com>
2024-03-21 07:53:28 +0100

    l

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import PostLetterGenerator from './PostLetterGenerator';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <PostLetterGenerator />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
